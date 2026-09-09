ac6a6818e7e18177bd31d0abebcde7579c6b4d61
Nicolas <adilsonq1976@gmail.com>
2026-09-08 21:46:25 -0300

    Exe A-R3

---
+++ ListaA_NicolasTamashiro/A-R3.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int compr(const char *str){
+    if(*str == '\0') return 0;
+
+    return 1 + compr(str + 1);
+}
+
+int main(){
+
+    char str[101];
+
+    scanf("%s", str);
+
+    printf("Comprimento: %d\n", compr(str));
+    
+    return 0;
+}
