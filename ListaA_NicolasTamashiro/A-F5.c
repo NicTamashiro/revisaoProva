80aeda573dbf0a884f9f000d54f4db925cc3cd21
Nicolas <adilsonq1976@gmail.com>
2026-09-08 21:32:35 -0300

    Arrumando exercicios

---
+++ ListaA_NicolasTamashiro/A-F5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void estatisticas(float vet[], int n, float * media, float * maior){
+    float soma = 0;
+    *maior = vet[0];
+
+    for (int i = 0; i < n; i++){
+        soma += vet[i];
+        if(vet[i] > *maior){
+            *maior = vet[i];
+        }
+    }
+
+    *media = soma / n;
+}
+
+int main(int argc, char *argv[]){
+    int n = atof(argv[1]);
+
+    float vet[n];
+
+    for (int i = 0; i < n; i++){
+        vet[i] = atof(argv[i + 2]);
+    }
+
+    float media, maior;
+    estatisticas(vet, n, &media, &maior);
+
+    printf("Media: %.1f\n", media);
+    printf("Maior: %.1f\n", maior);
+
+    return 0;
+}
