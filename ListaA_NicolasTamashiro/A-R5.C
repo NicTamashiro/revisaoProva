5a57a2a664e66f134f42c7919f54f45bd61cab6c
Nicolas <adilsonq1976@gmail.com>
2026-09-08 21:56:45 -0300

    Exe A-R5

---
+++ ListaA_NicolasTamashiro/A-R5.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void hanoi(int n, char origem, char destino, char auxiliar) {
+    if (n == 0) return;
+
+    hanoi(n - 1, origem, auxiliar, destino);
+
+    printf("%c -> %c\n", origem, destino);
+
+    hanoi(n - 1, auxiliar, destino, origem);
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    hanoi(n, 'A', 'C', 'B');
+    return 0;
+}
