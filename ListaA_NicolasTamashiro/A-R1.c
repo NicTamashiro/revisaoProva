46dd75e8323695284729aff5d09fe1dd490720d3
Nicolas <adilsonq1976@gmail.com>
2026-09-08 21:36:54 -0300

    Exe A-R1

---
+++ ListaA_NicolasTamashiro/A-R1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+long long fat(int n){
+    if(n == 0) return 1;
+
+    return n * fat(n-1);
+}
+
+int main(){
+
+    int x;
+    scanf("%d", &x);
+
+    printf("%lld\n", fat(x));
+
+    return 0;
+}
