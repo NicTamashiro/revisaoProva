a6d21ca015f45fd9ad75e213b9240c40fc420f19
Nicolas <adilsonq1976@gmail.com>
2026-09-16 21:08:46 -0300

    Exe B-R4

---
+++ ListaB_NicolasTamashiro/B-R4.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int mult(int a, int b){
+    if(b == 1) return a;
+
+    return a + mult(a, b-1);
+}
+
+int main(){
+
+    int a,b;
+    scanf("%d %d", &a, &b);
+
+    int resultado = mult(a, b);
+
+    printf("Produto: %d\n", resultado);
+
+    return 0;
+}
