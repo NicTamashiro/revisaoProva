9c69e190e9c7d0aa9f28283173bbb7dbcdc83b7e
Nicolas <adilsonq1976@gmail.com>
2026-09-18 19:59:18 -0300

    Exe C-R2

---
+++ ListaC_NicolasTamashiro/C-R2.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int pot(int x, int y){
+    if(y == 0) return 1;
+
+    return x * pot(x, y-1);
+}
+
+int main(){
+
+    int x,y;
+    scanf("%d %d", &x, &y);
+
+    printf("Resultado: %d\n", pot(x, y));
+
+}
